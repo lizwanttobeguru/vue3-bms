30a0ca5ae8d75f7f337ef3eb03eeff1af7c2fdd0
lizwanttobeguru <carpedemli@163.com>
2022-08-08 12:32:20 +0800

    first commit

---
+++ src/utils/axios.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import { ElMessage } from 'element-plus'
+import router from '@/router/index'
+import config from '~/config'
+import { localGet, localSet } from './index'
+
+
+
+axios.defaults.baseURL = config[import.meta.env.MODE].baseUrl
+axios.defaults.withCredentials = true
+// 请求头，headers 信息
+axios.defaults.headers['X-Requested-With'] = 'XMLHttpRequest'
+axios.defaults.headers['token'] = localGet('token') || ''
+// 默认 post 请求，使用 application/json 形式
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+
+// 请求拦截器，内部根据返回值，重新组装，统一管理
+axios.interceptors.response.use(res => {
+  if (typeof res.data !== 'object') {
+    ElMessage.error('服务端异常！')
+    return Promise.reject(res)
+  }
+  if (res.data.resultCode != 200) {
+    if (res.data.message) ElMessage.error(res.data.message)
+    if (res.data.resultCode == 419) {
+      router.push({ path: '/login' })
+    }
+    return Promise.reject(res.data)
+  }
+
+  return res.data.data
+})
+
+export default axios
---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import vitePluginImport from 'vite-plugin-babel-import'
+
+// https://vitejs.dev/config/
+export default ({ mode }) => defineConfig({
+  plugins: [
+    vue(),
+    vitePluginImport([
+      {
+        libraryName: 'element-plus',
+        libraryDirectory: 'es',
+        //    style(name) {
+        //      return `element-plus/dist/${name}.css`;
+        //     },
+      }
+    ])
+  ],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './'),
+      '@': path.resolve(__dirname, 'src')
+    },
+  },
+  base: './',
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://backend-api-02.newbee.ltd/manage-api/v1', // 凡是遇到 /api 路径的请求，都映射到 target 属性
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '') // 重写 api 为 空，就是去掉它
+      }
+    }
+  }
+})
